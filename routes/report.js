0ae2e222364b3741383dcb2fcba942ed8eb74de9
Andres Cruz <developer@andrescruz.io>
2020-09-15 11:38:24 -0700

    Implement report query

--- routes/report.js
+++ routes/report.js
@@ -4,6 +4,8 @@ let router = express.Router();
 const Joi = require('joi');
 const joiHandler = require('../handlers/joiHandler');
 
+const pool = require('../handlers/dbHandler');
+
 const reportSchema = Joi.object().keys({
     uuid: Joi.string().required().guid({version: ['uuidv4']}),
     fullName: Joi.string().required(),
@@ -15,12 +17,20 @@ router.post('/', function (req, res, next) {
     if (!joiHandler.validate(res, req.body, reportSchema)) return;
 
     const {uuid, fullName, errorCode, errorComment} = req.body;
-    console.log(uuid);
-    console.log(fullName);
 
-    return res.status(200).json({
-        message: 'Successfully logged report.'
-    })
+    pool.query('INSERT INTO report (uuid, fullName, errorCode, errorComment) VALUES(?, ?, ?, ?)',
+        [uuid, fullName, errorCode, errorComment], function (error, results, fields) {
+        if (error) {
+            return res.status(400).json({
+                message: 'Failed to log report.',
+                error: error.message
+            })
+        }
+
+        return res.status(200).json({
+            message: 'Successfully logged report.'
+        })
+    });
 });
 
 module.exports = router;
