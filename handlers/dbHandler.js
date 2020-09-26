aa794001132ed038989f438f51858fdd5f42f99d
facuevas <franciscuevas4@gmail.com>
2020-09-26 13:48:39 -0700

    Creating connections using the Sequelize ORM

--- handlers/dbHandler.js
+++ handlers/dbHandler.js
@@ -1,19 +1,37 @@
+const mysql = require('mysql');
 const dotenv = require('dotenv');
+const { Sequelize } = require('sequelize');
 dotenv.config();
 
-const mysql = require('mysql');
-const pool = mysql.createPool({
-    connectionLimit: 10,
+
+// const pool = mysql.createPool({
+//     connectionLimit: 10,
+//     host: 'localhost',
+//     user: process.env.STELLAR_USERNAME,
+//     password: process.env.STELLAR_PASSWORD,
+//     database: 'stellar',
+//     port: process.env.STELLAR_DB_PORT,
+// });
+
+/*
+ * Create the connection using sequelize ORM. 
+*/
+const sequelize = new Sequelize('stellar', process.env.STELLAR_USERNAME, process.env.STELLAR_PASSWORD, {
     host: 'localhost',
-    user: process.env.STELLAR_USERNAME,
-    password: process.env.STELLAR_PASSWORD,
-    database: 'stellar'
+    dialect: 'mysql',
+    operatorsAliases: false,
+    pool: {
+        max: 10,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    }
 });
 
-pool.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
-    if (error) throw error;
-    console.log('The solution is: ', results[0].solution);
-});
+const db = {};
+
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
 
 process.on("exit", function () {
     console.log("Shutting down.")
